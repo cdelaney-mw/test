d4637ebee1b90169a54571ec4819640c11bfb59c
cdelaney-mw <136635465+cdelaney-mw@users.noreply.github.com>
2024-09-12 10:09:31 -0400

    remove really long variable name

--- test.m
+++ test.m
@@ -1,3 +1,2 @@
 % issue for Code Analyzer to find
 8x3 = 24;
-aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaReallyLongVariableName = 12;
