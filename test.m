daa24af710f9b1fdf2355c6b3b0eb5e13e266ce8
cdelaney-mw <136635465+cdelaney-mw@users.noreply.github.com>
2024-12-04 15:37:28 -0500

    Fill array x

--- test.m
+++ test.m
@@ -1,4 +1,7 @@
 % issues for Code Analyzer to find
 8x3 = 24;
+for i = 1:10
+  x[i] = i
+end
 aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaReallyLongVariableName = 12;
 % test update
